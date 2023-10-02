e1ef210058b9032122c821c3bfc1634d60699dfa
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-10-02 22:46:32 +0700

    Crud Mongoose

--- app.js
+++ app.js
@@ -3,19 +3,38 @@
 // THIRD PARTY PACKAGE/MODULE
 const express = require("express")
 const morgan = require("morgan")
+const flash = require("connect-flash")
+const session = require("express-session")
 
 // OUR OWN PACKAGE/MODULE
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
+const adminRouter = require("./routes/adminRoutes")
 
 const app = express()
 
 // middleware dari express
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(morgan("dev"))
 // biar bisa baca static file
 app.use(express.static(`${__dirname}/public`))
+// setting view engine
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
 
+// flash and express session for notification
+app.use(
+  session({
+    secret: "geeksforgeeks",
+    saveUninitialized: true,
+    resave: true,
+  })
+)
+app.use(flash())
+
+// our own middleware example
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   console.log(req.requestTime)
@@ -24,5 +43,6 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
+app.use("/", adminRouter)
 
 module.exports = app
---
+++ controllers/adminController.js
@@ -0,0 +1,112 @@
+const Tour = require("../models/tourModel")
+
+const toursPage = async (req, res) => {
+  try {
+    console.log(req.query)
+    const { price, name, rating } = req.query
+
+    const condition = {}
+    if (price)
+      condition.price = { $gt: req.query.price }
+    if (name)
+      condition.name = {
+        $regex: ".*" + name.toLowerCase() + ".*",
+      }
+    if (rating)
+      condition.rating = { $gt: req.query.rating }
+
+    const tours = await Tour.find().where(
+      condition
+    )
+
+    res.render("tours/index.ejs", {
+      tours,
+      message: req.flash("message", ""),
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+const createPage = async (req, res) => {
+  try {
+    res.render("tours/create.ejs")
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+const createTour = async (req, res) => {
+  try {
+    await Tour.create(req.body)
+    req.flash("message", "Ditambah")
+    res.redirect("/dashboard")
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+const editPage = async (req, res) => {
+  try {
+    const tour = await Tour.findById(
+      req.params.id
+    )
+    res.render("tours/edit.ejs", {
+      tour,
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+const editTour = async (req, res) => {
+  try {
+    const id = req.params.id
+    await Tour.findByIdAndUpdate(id, req.body, {
+      new: true,
+    })
+    req.flash("message", "Diupdate")
+    res.redirect("/dashboard")
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+const removeTour = async (req, res) => {
+  try {
+    const id = req.params.id
+    await Tour.findByIdAndRemove(id)
+    req.flash("message", "Dihapus")
+    res.redirect("/dashboard")
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      message: err.message,
+    })
+  }
+}
+
+module.exports = {
+  toursPage,
+  createPage,
+  createTour,
+  editPage,
+  editTour,
+  removeTour,
+}
